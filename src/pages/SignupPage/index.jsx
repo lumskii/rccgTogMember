15e8fd37bdd09d9697c0ceaf080b8d577d2c5b96
lumskii <67390011+lumskii@users.noreply.github.com>
2023-10-06 14:40:43 -0700

    sign up and login page

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -2,20 +2,27 @@ import {
   Avatar,
   Box,
   Button,
-  Checkbox,
   Container,
   CssBaseline,
+  FormControl,
   FormControlLabel,
+  FormLabel,
   Grid,
+  InputLabel,
   Link,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
   TextField,
   Typography,
 } from "@mui/material";
 import { LockOutlined } from "@mui/icons-material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import React from "react";
+import React, { useState } from "react";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { IMaskInput } from "react-imask";
 
 function Copyright(props) {
   return (
@@ -25,9 +32,12 @@ function Copyright(props) {
       align="center"
       {...props}
     >
-        Copyright {" "}
-      {"© "}
-      <Link color="inherit" href="https://rccgthog.org/" sx={{textDecoration: 'none'}}>
+      Copyright {"© "}
+      <Link
+        color="inherit"
+        href="https://rccgthog.org/"
+        sx={{ textDecoration: "none" }}
+      >
         RCCGTOG
       </Link>{" "}
       {new Date().getFullYear()}
@@ -38,7 +48,42 @@ function Copyright(props) {
 
 const theme = createTheme();
 
+const PhoneField = React.forwardRef(function PhoneField(props, ref) {
+  const { onChange, ...other } = props;
+  return (
+    <IMaskInput
+      {...other}
+      mask="(000) 000-0000"
+      definitions={{
+        "#": /[0-9]/,
+      }}
+      inputRef={ref}
+      onAccept={(value) => onChange({ target: { name: props.name, value } })}
+      overwrite
+    />
+  );
+});
+
 function Signup() {
+  const [gender, setGender] = useState("");
+  const [howDidYouHear, setHowDidYouHear] = useState("");
+  const [phone, setPhone] = useState({
+    textmask: "(100) 000-0000",
+    numberformat: "1320",
+  });
+
+  const handlePhoneChange = (event) => {
+    setPhone({
+      ...phone,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+    setHowDidYouHear(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -51,98 +96,160 @@ function Signup() {
 
   return (
     <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlined />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="given-name"
-                name="firstName"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="family-name"
-              />
-            </Grid>
-            <Grid item xs={12}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker label="Date of Birth" sx={{ width: '100%' }} />
-                </LocalizationProvider>
-            </Grid>
-            
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
           >
-            Sign Up
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker label="Date of Birth" sx={{ width: "100%" }} />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Gender</FormLabel>
+                  <RadioGroup
+                    name="gender"
+                    value={gender}
+                    onChange={handleGenderChange}
+                    row
+                  >
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Male"
+                    />
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Female"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Phone Number"
+                  onChange={handlePhoneChange}
+                  value={phone.textmask}
+                  name="textmask"
+                  id="phone"
+                  InputProps={{
+                    inputComponent: PhoneField,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel id="how-did-you-hear-label">
+                  How did you hear about us?
+                </InputLabel>
+                <Select
+                  labelId="how-did-you-hear-label"
+                  id="how-did-you-hear"
+                  value={howDidYouHear}
+                  onChange={handleGenderChange}
+                  label="How did you hear about us?"
+                  fullWidth
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="Social Media">Social Media</MenuItem>
+                  <MenuItem value="Friend or Family">Friend or Family</MenuItem>
+                  <MenuItem value="Online Search">Online Search</MenuItem>
+                  <MenuItem value="Advertisement">Advertisement</MenuItem>
+                  <MenuItem value="Other">Other</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                    id='access-code'
+                    label='Access Code'
+                    type='password'
+                    fullWidth
+                />
+              </Grid>
             </Grid>
-          </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
-      </Box>
-      <Copyright sx={{ mt: 5 }} />
-    </Container>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
     </ThemeProvider>
   );
 }
